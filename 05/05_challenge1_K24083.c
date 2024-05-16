a1f3b5d575fe50335f48908415b604385fbc462d
nasubi916 <r916nis@gmai.com>
2024-05-16 18:35:45 +0900

    05 fix

--- 05/05_challenge1_K24083.c
+++ 05/05_challenge1_K24083.c
@@ -12,10 +12,11 @@ int main(int argc, const char* argv[]) {
   printf("時間帯? ");
   scanf("%d", &time);
 
-  if (isOpen[dayOfWeek][time])
+  if (dayOfWeek == 1 || dayOfWeek == 2 ||  (dayOfWeek == 3 && time == 0) || dayOfWeek ==4 || dayOfWeek == 5 || (dayOfWeek == 6  && time == 1)) {
     printf("診察を行っています\n");
-  else
+  } else {
     printf("診察は行っていません\n");
+  }
 
   return 0;
 }
