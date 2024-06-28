4e11ed640d21bd6303be27cbb45b88e00283d416
nasubi <r916nis@gmail.com>
2024-06-29 03:15:04 +0900

    重力方向の変更

---
+++ final/gravity.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+
+// 1:↓ 2:← 3:↑ 4:→
+// 渡された配列をnowGravityの方向に寄せる
+int changeGravity(int board[7][7], int nextGravity)
+{
+  int index = 0;
+
+
+      printf("Change gravity!!!\n");
+      printf("1:↓ 2:← 3:↑ 4:→\n");
+
+  // 1:↓
+  if (nextGravity == 1)
+  {
+    // 列ごとに下に詰める
+    for (int i = 0; i < 7; i++)
+    {
+      for (int j = 0; j < 7; j++)
+      {
+        // 0である場合は1or2をその列の中から探し､入れ替える
+        if (board[j][i] != 0) continue;
+        index = j;
+        for (int k = j + 1; k < 7; k++)
+        {
+          if (board[k][i] != 0)
+          {
+            board[index][i] = board[k][i];
+            board[k][i] = 0;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // 2:←
+  if (nextGravity == 2)
+  {
+    // 行ごとに右に詰める
+    for (int i = 0; i < 7; i++)
+    {
+      for (int j = 0; j < 7; j++)
+      {
+        // 0である場合は1or2をその列の中から探し､入れ替える
+        if (board[i][j] != 0)
+          continue;
+        index = j;
+        for (int k = j + 1; k < 7; k++)
+        {
+          if (board[i][k] != 0)
+          {
+            board[i][index] = board[i][k];
+            board[i][k] = 0;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // 3:↑
+  if (nextGravity == 3)
+  {
+    // 列ごとに上に詰める
+    for (int i = 0; i < 7; i++)
+    {
+      for (int j = 0; j < 7; j++)
+      {
+        // 0である場合は1or2をその列の中から探し､入れ替える
+        if (board[j][i] != 0)
+          continue;
+        index = j;
+        for (int k = j + 1; k < 7; k++)
+        {
+          if (board[k][i] != 0)
+          {
+            board[index][i] = board[k][i];
+            board[k][i] = 0;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // 4:→
+  if (nextGravity == 4)
+  {
+    // 行ごとに左に詰める
+    for (int i = 0; i < 7; i++)
+    {
+      for (int j = 6; j >= 0; j--)
+      {
+        // 0である場合は1or2をその列の中から探し､入れ替える
+        if (board[i][j] != 0)
+          continue;
+        index = j;
+        for (int k = j - 1; k >= 0; k--)
+        {
+          if (board[i][k] != 0)
+          {
+            board[i][index] = board[i][k];
+            board[i][k] = 0;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  return 0;
+}
--- final/main.c
+++ final/main.c
@@ -2,28 +2,50 @@
 #include "./board.c"
 #include "./action.c"
 #include "./check.c"
+#include "./gravity.c"
 
 int main(void)
 {
-  // タイトル 後でかっこよく
-  printf("Connect four + gravity\n");
-
   // 0=empty, 1=player1, 2=player2
   int board[7][7] = {0};
-  // 0=continue, 1=player1, 2=player2
+  // 0=continue, 1=player1, 2=player2 3=draw
   int isGameFin = 0;
+  // 1:↓ 2:← 3:↑ 4:→
+  int nowGravity = 1;
   int move = 1;
+
+  // タイトル 後でかっこよく
+  printf("Connect four + gravity\n");
+  // printf("Start game? [y/N]\n");
 
   ViewBoard(board);
   while (isGameFin == 0)
   {
-    printf("turn %d\n", move);
+    printf("move: %d ", move);
+    printf("next gravity: %d\n", nowGravity);
+
     playerAction(board, move % 2 + 1);
+    printf("\n\n\n");
+
     ViewBoard(board);
+    //! test 3手目で重力変更
+    if (move % 3 == 0)
+    {
+
+      changeGravity(board, nowGravity % 4 + 1);
+      nowGravity++;
+      ViewBoard(board);
+    }
     isGameFin = checkGameSet(board);
+
+    // 49手目で引き分け
+    if (move == 49)
+      isGameFin = 3;
     move++;
   }
 
+  // わかりやすくするためにグラフィカルに表示
+  printf("\n\n\n\n\n");
   printf("Game set Winner: %d\n", isGameFin);
   if (isGameFin == 1)
   {
