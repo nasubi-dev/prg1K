6405a707071fc7f98cbc016c4594e2432f4a2487
nasubi <r916nis@gmail.com>
2024-06-29 03:04:41 +0900

    斜め上がりの判定追加 行番号表示を見やすく

--- final/board.c
+++ final/board.c
@@ -2,27 +2,21 @@
 
 int ViewBoard(int board[7][7])
 {
-  printf("  1  2  3  4  5  6  7\n");
+  // 列番号表示
+  printf("   1  2  3  4  5  6  7\n");
 
   for (int i = 0; i < 7; i++)
   {
-    printf("%d", i + 1);
+    // 行番号表示
+    printf("%d ", i + 1);
+
     for (int j = 0; j < 7; j++)
     {
       printf("[");
 
-      if (board[i][j] == 1)
-      {
-        printf("X");
-      }
-      else if (board[i][j] == 2)
-      {
-        printf("O");
-      }
-      else
-      {
-        printf(" ");
-      }
+      if (board[i][j] == 1) printf("X");
+      else if (board[i][j] == 2) printf("O");
+      else printf(" ");
 
       printf("]");
     };
--- final/check.c
+++ final/check.c
@@ -34,7 +34,7 @@ int checkGameSet(int board[7][7])
     }
   }
 
-  // 斜め
+  // 斜め下がり
   for (int i = 0; i < 4; i++)
   {
     for (int j = 0; j < 4; j++)
@@ -44,6 +44,22 @@ int checkGameSet(int board[7][7])
         return 1;
       }
       else if (board[i][j] == 2 && board[i + 1][j + 1] == 2 && board[i + 2][j + 2] == 2 && board[i + 3][j + 3] == 2)
+      {
+        return 2;
+      }
+    }
+  }
+
+  // 斜め上がり
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 3; j < 7; j++)
+    {
+      if (board[i][j] == 1 && board[i + 1][j - 1] == 1 && board[i + 2][j - 2] == 1 && board[i + 3][j - 3] == 1)
+      {
+        return 1;
+      }
+      else if (board[i][j] == 2 && board[i + 1][j - 1] == 2 && board[i + 2][j - 2] == 2 && board[i + 3][j - 3] == 2)
       {
         return 2;
       }
